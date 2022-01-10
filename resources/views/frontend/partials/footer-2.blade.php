1a57cc0f6e9d12a3b0218d4f99806f3962f0351f
Pawan Rai <rahnn3@gmail.com>
2022-01-10 21:47:21 +0545

    footer

--- resources/views/frontend/partials/footer-2.blade.php
+++ resources/views/frontend/partials/footer-2.blade.php
@@ -117,7 +117,7 @@
                     </a>
                 </div>
 
-                <div class="alert" role="alert" id="notification">
+                <div class="alert" role="alert" id="notification" style="display: none">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
